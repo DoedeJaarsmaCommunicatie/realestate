e82104b1b05e80cea07270597e83f4657202962f
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-02-24 14:13:01 +0100

    Fixed "Makelaar" not "Makelaardij"

--- routes/realtor.php
+++ routes/realtor.php
@@ -4,54 +4,54 @@ namespace App\Routes;
 
 use App\Controllers\Http\Vacancy\RealtorVacancyController;
 
-// Base makelaardij
-\Routes::map('vacatures/makelaardij', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/regio/:reg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/vakgebied/:vg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/organisatie/:org', [RealtorVacancyController::class, 'route']);
+// Base makelaar
+\Routes::map('vacatures/makelaar', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/regio/:reg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/vakgebied/:vg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/organisatie/:org', [RealtorVacancyController::class, 'route']);
 
-// Double makelaardij
-\Routes::map('vacatures/makelaardij/regio/:reg/vakgebied/:vg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/regio/:reg/organisatie/:org', [RealtorVacancyController::class, 'route']);
+// Double makelaar
+\Routes::map('vacatures/makelaar/regio/:reg/vakgebied/:vg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/regio/:reg/organisatie/:org', [RealtorVacancyController::class, 'route']);
 
-\Routes::map('vacatures/makelaardij/vakgebied/:vg/regio/:reg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/vakgebied/:vg/organisatie/:org', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/vakgebied/:vg/regio/:reg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/vakgebied/:vg/organisatie/:org', [RealtorVacancyController::class, 'route']);
 
-\Routes::map('vacatures/makelaardij/organisatie/:org/regio/:reg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/organisatie/:org/vakgebied/:vg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/organisatie/:org/regio/:reg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/organisatie/:org/vakgebied/:vg', [RealtorVacancyController::class, 'route']);
 
-// Triple Makelaardij
-\Routes::map('vacatures/makelaardij/regio/:reg/vakgebied/:vg/organisatie/:org', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/regio/:reg/organisatie/:org/vakgebied/:vg', [RealtorVacancyController::class, 'route']);
+// Triple Makelaar
+\Routes::map('vacatures/makelaar/regio/:reg/vakgebied/:vg/organisatie/:org', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/regio/:reg/organisatie/:org/vakgebied/:vg', [RealtorVacancyController::class, 'route']);
 
-\Routes::map('vacatures/makelaardij/organisatie/:org/vakgebied/:vg/regio/:reg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/organisatie/:org/regio/:reg/vakgebied/:vg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/organisatie/:org/vakgebied/:vg/regio/:reg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/organisatie/:org/regio/:reg/vakgebied/:vg', [RealtorVacancyController::class, 'route']);
 
-\Routes::map('vacatures/makelaardij/vakgebied/:vg/organisatie/:org/regio/:reg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/vakgebied/:vg/regio/:reg/organisatie/:org', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/vakgebied/:vg/organisatie/:org/regio/:reg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/vakgebied/:vg/regio/:reg/organisatie/:org', [RealtorVacancyController::class, 'route']);
 
-// Paginated makelaardij
-\Routes::map('vacatures/makelaardij/page/:pg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/vakgebied/:vg/page/:pg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/organisatie/:org/page/:pg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/organisatie/:org/vakgebied/:vg/page/:pg', [RealtorVacancyController::class, 'route']);
+// Paginated makelaar
+\Routes::map('vacatures/makelaar/page/:pg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/vakgebied/:vg/page/:pg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/organisatie/:org/page/:pg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/organisatie/:org/vakgebied/:vg/page/:pg', [RealtorVacancyController::class, 'route']);
 
-// Double paginated makelaardij
-\Routes::map('vacatures/makelaardij/regio/:reg/vakgebied/:vg/page/:pg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/regio/:reg/organisatie/:org/page/:pg', [RealtorVacancyController::class, 'route']);
+// Double paginated makelaar
+\Routes::map('vacatures/makelaar/regio/:reg/vakgebied/:vg/page/:pg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/regio/:reg/organisatie/:org/page/:pg', [RealtorVacancyController::class, 'route']);
 
-\Routes::map('vacatures/makelaardij/vakgebied/:vg/regio/:reg/page/:pg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/vakgebied/:vg/organisatie/:org/page/:pg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/vakgebied/:vg/regio/:reg/page/:pg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/vakgebied/:vg/organisatie/:org/page/:pg', [RealtorVacancyController::class, 'route']);
 
-\Routes::map('vacatures/makelaardij/organisatie/:org/regio/:reg/page/:pg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/organisatie/:org/vakgebied/:vg/page/:pg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/organisatie/:org/regio/:reg/page/:pg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/organisatie/:org/vakgebied/:vg/page/:pg', [RealtorVacancyController::class, 'route']);
 
-// Triple paginated Makelaardij
-\Routes::map('vacatures/makelaardij/regio/:reg/vakgebied/:vg/organisatie/:org/page/:pg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/regio/:reg/organisatie/:org/vakgebied/:vg/page/:pg', [RealtorVacancyController::class, 'route']);
+// Triple paginated Makelaar
+\Routes::map('vacatures/makelaar/regio/:reg/vakgebied/:vg/organisatie/:org/page/:pg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/regio/:reg/organisatie/:org/vakgebied/:vg/page/:pg', [RealtorVacancyController::class, 'route']);
 
-\Routes::map('vacatures/makelaardij/organisatie/:org/vakgebied/:vg/regio/:reg/page/:pg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/organisatie/:org/regio/:reg/vakgebied/:vg/page/:pg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/organisatie/:org/vakgebied/:vg/regio/:reg/page/:pg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/organisatie/:org/regio/:reg/vakgebied/:vg/page/:pg', [RealtorVacancyController::class, 'route']);
 
-\Routes::map('vacatures/makelaardij/vakgebied/:vg/organisatie/:org/regio/:reg/page/:pg', [RealtorVacancyController::class, 'route']);
-\Routes::map('vacatures/makelaardij/vakgebied/:vg/regio/:reg/organisatie/:org/page/:pg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/vakgebied/:vg/organisatie/:org/regio/:reg/page/:pg', [RealtorVacancyController::class, 'route']);
+\Routes::map('vacatures/makelaar/vakgebied/:vg/regio/:reg/organisatie/:org/page/:pg', [RealtorVacancyController::class, 'route']);
